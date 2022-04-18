73d7cffea0b8d4b05dff41d07589c37a281095d8
ageru <ageru@tokyoite.mobi>
2022-04-18 11:51:40 +0200

    Correction connection VM: nom d'hote

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -168,7 +168,7 @@ resource "null_resource" "bringup_trigger" {
         connection {
           type = "ssh"
           user = var.username_trigger
-          host = data.azurerm_network_interface.nic1_trigger.private_ip_address
+          host = azurerm_network_interface.nic1_trigger.private_ip_address
           port = "22"
           timeout = "1m"
           private_key = file(var.sshprivatekey_trigger)
