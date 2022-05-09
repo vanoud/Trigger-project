4855cd13e0a00dbbfd862832f7b99880607390e6
ageru <ageru@tokyoite.mobi>
2022-05-09 14:29:23 +0200

    ajout des descriptions et types dans variables.tf

--- deploy/Azure/terraform/variables.tf
+++ deploy/Azure/terraform/variables.tf
@@ -1,8 +1,32 @@
-variable "subscription_id" {}
-variable "tenant_id" {}
-variable "ressource_loc" {}
-variable "vm_size" {}
-variable "vm_disk_type" {}
-variable "username_trigger" {}
-variable "sshpubkey_trigger" {}
-variable "sshprivatekey_trigger" {}
+variable "subscription_id" {
+    description = "Fourni par Azure sous le nom 'id' ou 'Subscription ID'"
+    type = string
+}
+variable "tenant_id" {
+        description = "Fourni par Azure sous le nom 'homeTenantId' ou 'Parent management group'"
+    type = string
+}
+variable "ressource_loc" {
+        description = "Domiciliation géographique du déploiement, à choisir sur Azure"
+    type = string
+}
+variable "vm_size" {
+        description = "SKU/taille de la VM, à choisir sur Azure"
+    type = number
+}
+variable "vm_disk_type" {
+        description = "SKU/type de stockage pour l'IS de la VM, à choisir sur Azure"
+    type = string
+}
+variable "username_trigger" {
+        description = "Nom d'utilisateur pour la VM, à choisir"
+    type = string
+}
+variable "sshpubkey_trigger" {
+        description = "Chemin d'accès du fichier clef public SSH à importer sur la VM, clef RSA uniquement"
+    type = string
+}
+variable "sshprivatekey_trigger" {
+        description = "Chemin d'accès du fichier clef privé SSH pour accéder à la VM, clef RSA uniquement"
+    type = string
+}
